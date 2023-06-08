e2b008cffeb6505c5a09931679a4f96c195137e1
Stefano-BML <stefanoml1128@gmail.com>
2023-06-07 21:03:50 -0700

    error sintexis

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = 0, 0
-        else:
-            tuple_b = tuple_b[0], 0
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        print("")
