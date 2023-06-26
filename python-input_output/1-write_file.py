6ee8c4d28bd58bc58e9d0854187645b0f7405775
Stefano-BML <stefanoml1128@gmail.com>
2023-06-25 19:38:28 -0700

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-# 1-number_of_lines.py
+# 1-write_file.py
 # stefano madueno
-"""Defines a text file line-counting function."""
+"""Defines a file-writing function."""
 
 
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
-    with open(filename) as f:
-        for line in f:
-            lines += 1
-    return lines
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
