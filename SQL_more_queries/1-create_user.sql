72d7621bfa63648666c90f2eeb2fd9588fd3f8c1
Stefano-BML <stefanoml1128@gmail.com>
2023-07-30 10:32:49 -0700

    fix: create userpermissions :rocket:

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,3 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grants all privileges on all databases to user_0d_1.
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
+-- creates a user and gives it all permissions with a password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; 
