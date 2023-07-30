b3911a1ffd99576f2e34d5320797199f5c9365d0
Stefano-BML <stefanoml1128@gmail.com>
2023-07-30 10:28:48 -0700

    fix: create user options :rocket:

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,9 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   WITH GRANT OPTION;
-
+-- Grants all privileges on all databases to user_0d_1.
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
