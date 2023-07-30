08ddb0d3f9386b218c0c08ccbed5e8cc12428e90
Stefano-BML <stefanoml1128@gmail.com>
2023-07-30 10:25:45 -0700

    fix: create read user :rocket:

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,13 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+-- Creates the database hbtn_0d_2 if it doesn't exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Creates the user user_0d_2 if it doesn't exist and sets the password.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grants USAGE privilege on all databases to user_0d_2.
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+
+-- Grants SELECT privilege on hbtn_0d_2 database to user_0d_2.
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+
