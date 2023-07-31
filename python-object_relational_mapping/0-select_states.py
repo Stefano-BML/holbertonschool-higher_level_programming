8d774f8bf4c248aeb2cf4bfa092955836979cd88
Stefano-BML <stefanoml1128@gmail.com>
2023-07-30 23:20:49 -0700

    fix: another option by states usa :rocket:

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
 # Lists all states from the database hbtn_0e_0_usa.
-# by Stefano Madueno
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
 import MySQLdb
 
-def list_states(username, password, database):
-    # Establecer la conexión con la base de datos
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
-    cursor = db.cursor()
-
-    # Ejecutar la consulta para obtener los estados ordenados por su id
-    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
-
-    # Mostrar los resultados en el formato especificado
-    for row in cursor.fetchall():
-        print(row)
-
-    # Cerrar la conexión
-    cursor.close()
-    db.close()
-
 if __name__ == "__main__":
-    # Verificar que se proporcionaron los 3 argumentos requeridos
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Llamar a la función para listar los estados
-    list_states(username, password, database)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
