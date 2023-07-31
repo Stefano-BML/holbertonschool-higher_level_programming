550294dae253f15c31f185d4d4b4b697c8429a08
Stefano-BML <stefanoml1128@gmail.com>
2023-07-30 22:36:22 -0700

    fix: execute funtions :rocket:

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,5 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-
-    for state in c.fetchall():
-        print(state)
-
-    c.close()
-    db.close()
-
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
