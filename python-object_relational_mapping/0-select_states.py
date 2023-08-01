3a67f9c3ef233140ed0a398c5c985ff61e9d9ae7
Stefano-BML <stefanoml1128@gmail.com>
2023-07-31 21:17:53 -0700

    fix: clean and localhost :rocket:

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,24 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
 
     c = db.cursor()
     c.execute("SELECT * FROM states;")
-    states = cur.fetchall()
+    states = c.fetchall()
 
     for state in states:
         print(state)
 
+    # Close cursor
     c.close()
+
+    # close database
     db.close()
+
