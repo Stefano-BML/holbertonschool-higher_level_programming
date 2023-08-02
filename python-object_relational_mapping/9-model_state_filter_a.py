22d812ee23b3b063dba21b95c5609c7ba8bd28b8
Stefano-BML <stefanoml1128@gmail.com>
2023-08-02 00:09:49 -0700

    fix: try another :rocket:

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,26 +5,23 @@ the letter `a` from the database `hbtn_0e_6_usa`.
 """
 
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(bind=engine)
-
-    q = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
-
-    for instance in q:
-        print("{}: {}".format(instance.id, instance.name))
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    match = '%a%'
+    re = session.query(State).filter(State.name.like(match)).order_by(State.id)
+    for x in re:
+        print("{}: {}".format(x.id, x.name))
+    session.close()
