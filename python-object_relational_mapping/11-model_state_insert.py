626147698c803bab94696763f5f0601331ee770f
Stefano-BML <stefanoml1128@gmail.com>
2023-08-02 00:12:29 -0700

    fix: T11 prints the state object :rocket:

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -7,25 +7,21 @@ to the database `hbtn_0e_6_usa`.
 import sys
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(bind=engine)
-
-    new = State(name="Louisiana")
-    session.add(new)
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    new_State = State(name="Louisiana")
+    re = session.add(new_State)
     session.commit()
-
-    print(new.id)
+    print(new_State.id)
+    session.close()
