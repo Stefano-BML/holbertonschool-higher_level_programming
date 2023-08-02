8f37dc3af92828b1764f091f7284a070b3edfef1
Stefano-BML <stefanoml1128@gmail.com>
2023-08-02 00:15:10 -0700

    fix: deletes all state objects :rocket:

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,24 +7,21 @@ the letter `a` from the database `hbtn_0e_6_usa`.
 import sys
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(bind=engine)
-
-    q = session.query(State).filter(State.name.like('%a%'))
-    q.delete(synchronize_session=False)
-
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    re = session.query(State).filter(State.name.like('%a%'))
+    for row in re:
+        session.delete(row)
     session.commit()
+    session.close()
