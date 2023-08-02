2586a590fe86a6cd2db31870fc98e26b97b0587c
Stefano-BML <stefanoml1128@gmail.com>
2023-08-02 00:06:44 -0700

    fix: try another idea :rocket:

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,28 +4,24 @@ Script that prints the first `State` object from the database `hbtn_0e_6_usa`.
 """
 
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(bind=engine)
-
-    instance = session.query(State).order_by(State.id).first()
-
-    if instance:
-        print("{}: {}".format(instance.id, instance.name))
-    else:
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    re = session.query(State).order_by(State.id).first()
+    if re is None:
         print("Nothing")
+    else:
+        print("{}: {}".format(re.id, re.name))
+    session.close()
